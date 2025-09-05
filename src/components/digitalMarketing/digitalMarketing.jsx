f882becca08a0753356f9a53c757a1c15a679538
himanshusain0 <ht0257445@gmail.com>
2025-09-06 00:58:26 +0530

    .

--- src/components/digitalMarketing/digitalMarketing.jsx
+++ src/components/digitalMarketing/digitalMarketing.jsx
@@ -38,7 +38,7 @@ function DigitalMarketing() {
           <div className="col-md-6 text-center mb-4 mb-md-0">
             <img src={DevImage} alt="Digital Marketing" className="img-fluid" />
             <div className="bg-warning text-white p-4 mt-4 fw-bold rounded">
-              "We don't just run ads, we grow your business online"
+              "We don't just run ads, we grow your business online s"
             </div>
           </div>
 
